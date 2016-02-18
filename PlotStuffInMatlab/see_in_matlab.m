6ecbafebe00a5f6f8aeb589f41fff4c19d5202dd
potaopereira <pedro.m.otao.pereira@gmail.com>
2016-02-18 18:21:29 +0100

    see_in_matlab.m now creates avi movie with data

--- PlotStuffInMatlab/see_in_matlab.m
+++ PlotStuffInMatlab/see_in_matlab.m
@@ -2,7 +2,6 @@ format long
 
 % read text file: change name accordingly
 M = dlmread('_data_0.txt');
-M = dlmread('_data_3.txt');
 
 time       = M(:,1);
 state      = M(:,2:13);
@@ -23,64 +22,283 @@ set(0,'DefaultTextInterpreter','latex',...
       'defaulttextfontsize',12,...
       'defaultaxesfontsize',12);
 
+%% make video with Video Writer
+
+%clc
+close all
+figure
+
+video = VideoWriter('Trajectory.avi');
+video.FrameRate = 1;
+open(video);
+
+% put initial time to 0
+time = time - time(1);
+% final time
+tF = time(end);
+
+% time window for the plotting
+t0_desired    = 0;
+tF_desired    = 20;
+time_interval = tF_desired - t0_desired;
+
+times_to_plot = time >= t0_desired & time <= tF_desired;
+times_vector  = time(times_to_plot);
+
+
+% position and velocity of the quadrotor 
+pQ  = state(times_to_plot,7:9);
+vQ  = state(times_to_plot,10:12);
+
+% position and velocity of the load
+pM  = state(times_to_plot,1:3);
+vM  = state(times_to_plot,4:6);
+
+
+% desired position of the load
+pMd = stated(times_to_plot,1:3);
+
+% number of times we want to plot quadrotor vehicle
+num = 20;
+marker = zeros(num+1,1);
+for i = 0:1:num
+    [minmin, index] = min(abs(times_vector - time_interval*i/num));
+    marker(i+1)     = index;
+end
+
+
+for j = 1:num + 1
+    
+    hold on
+    % plot load trajectory
+    plot3(pM(:,1) ,pM(:,2) ,pM(:,3) ,'Color',[0.7 0.7 0.7],'Linewidth',2)
+    % plot load desired trajectory
+    plot3(pMd(:,1),pMd(:,2),pMd(:,3),'Color',[0.3 0.3 0.3],'Linewidth',1)    
+    
+    % position of quadrotor
+    pp   = pQ(marker(j),:)';
+    
+    % position of load
+    pp2  = pM(marker(j),:)';
+
+    % cable unit vector 
+    nn   = (pp - pp2)/norm(pp - pp2);
+    
+    % rotation matrix associated to cable
+    RR   = [null(nn') nn];
+    
+    % plot quadrotor drawing
+    %DISC_plot(pp,RR,1,[0.8 0.8 0.8],0.5,0.1)
+    %plot_vect(pp,pp + RR*[0;0;1]*0.1,[0.8 0.8 0.8])  
+    quad_plot(pp,RR,1,[0.5 0.5 0.5],0.1,0.1)
+     
+    % draw cable between load and quadrotor
+    Connect(pp',pp2')  
+
+    % load desired position 
+    pp2 = pMd(marker(j),:)';
+    % plot load desired position
+    plot3(pp2(1),pp2(2),pp2(3),'o','Color','b')
+   
+    xlabel('x (m)')
+    ylabel('y (m)')
+    zlabel('z (m)')
+    
+    xlim([-1.5,1.5])
+    ylim([-1.5,1.5])
+    zlim([0,1.2])
+    % axis equal
+    % axis tight    
+    
+    view(56,16)
+
+    % Store the frame
+    writeVideo(video,getframe(gca, [0 0 435 344]));
+    
+    close all
+
+end
+
+close(video);  
+  
+  
+%% make video
+%clc
+close all
+figure
+
+% put initial time to 0
+time = time - time(1);
+% final time
+tF = time(end);
+
+% time window for the plotting
+t0_desired    = 0;
+tF_desired    = 20;
+time_interval = tF_desired - t0_desired;
+
+times_to_plot = time >= t0_desired & time <= tF_desired;
+times_vector  = time(times_to_plot);
+
+
+% position and velocity of the quadrotor 
+pQ  = state(times_to_plot,7:9);
+vQ  = state(times_to_plot,10:12);
+
+% position and velocity of the load
+pM  = state(times_to_plot,1:3);
+vM  = state(times_to_plot,4:6);
+
+
+% desired position of the load
+pMd = stated(times_to_plot,1:3);
+
+
+% number of times we want to plot quadrotor vehicle
+num = 20;
+marker = zeros(num+1,1);
+for i = 0:1:num
+    [minmin, index] = min(abs(times_vector - time_interval*i/num));
+    marker(i+1)     = index;
+end
+
+
+for j = 1:num + 1
+    
+    hold on
+    % plot load trajectory
+    plot3(pM(:,1) ,pM(:,2) ,pM(:,3) ,'Color',[0.7 0.7 0.7],'Linewidth',2)
+    % plot load desired trajectory
+    plot3(pMd(:,1),pMd(:,2),pMd(:,3),'Color',[0.3 0.3 0.3],'Linewidth',1)    
+    
+    % position of quadrotor
+    pp   = pQ(marker(j),:)';
+    
+    % position of load
+    pp2  = pM(marker(j),:)';
+
+    % cable unit vector 
+    nn   = (pp - pp2)/norm(pp - pp2);
+    
+    % rotation matrix associated to cable
+    RR   = [null(nn') nn];
+    
+    % plot quadrotor drawing
+    %DISC_plot(pp,RR,1,[0.8 0.8 0.8],0.5,0.1)
+    %plot_vect(pp,pp + RR*[0;0;1]*0.1,[0.8 0.8 0.8])  
+    quad_plot(pp,RR,1,[0.5 0.5 0.5],0.1,0.1)
+     
+    % draw cable between load and quadrotor
+    Connect(pp',pp2')  
+
+    % load desired position 
+    pp2 = pMd(marker(j),:)';
+    % plot load desired position
+    plot3(pp2(1),pp2(2),pp2(3),'o','Color','b')
+   
+    xlabel('x (m)')
+    ylabel('y (m)')
+    zlabel('z (m)')
+    
+    xlim([-1.5,1.5])
+    ylim([-1.5,1.5])
+    zlim([0,1.2])
+    % axis equal
+    % axis tight    
+    
+    view(56,16)
+
+    % Store the frame
+    % M_frame(j) = getframe(ax); % leaving 
+    M_frame(j) = getframe(gca, [0 0 435 344]);
+    
+    close all
+
+end
+
+% movie(M_frame,1,1)
+movie2avi(M_frame,'Trajectory.avi','fps',1);  
+% VideoWriter(....,...);  
+
+  
 %%
 clc
 close all
 figure
 
+% put initial time to 0
+time = time - time(1);
+% final time
+tF = time(end);
+
+% time window for the plotting
+t0_desired    = 0;
+tF_desired    = 5;
+time_interval = tF_desired - t0_desired;
+
+times_to_plot = time >= t0_desired & time <= tF_desired;
+times_vector  = time(times_to_plot);
+
+
 t = time - time(1);
 tEND = t(end);
 
-pQ  = state(:,7:9);
-vQ  = state(:,10:12);
 
-pM  = state(:,1:3);
-vM  = state(:,4:6);
+% position and velocity of the quadrotor 
+pQ  = state(times_to_plot,7:9);
+vQ  = state(times_to_plot,10:12);
 
-% UU  = Actuation.signals(1).values;
+% position and velocity of the load
+pM  = state(times_to_plot,1:3);
+vM  = state(times_to_plot,4:6);
 
-pMd = stated(:,1:3);
-% ades = pd.Data(:,7:9);
+
+% desired position of the load
+pMd = stated(times_to_plot,1:3);
 
 
 hold on
+% plot load trajectory
 plot3(pM(:,1) ,pM(:,2) ,pM(:,3) ,'Color','k','Linewidth',2)
+% plot load desired trajectory
 plot3(pMd(:,1),pMd(:,2),pMd(:,3),'Color',[0.3 0.3 0.3],'Linewidth',1)
 
+% number of times we want to plot quadrotor vehicle
 num = 4;
-II = zeros(num+1,1);
+marker = zeros(num+1,1);
 for i = 0:1:num
-    [minmin, index] = min(abs(t-tEND*i/num));
-    II(i+1) = index;
+    [minmin, index] = min(abs(times_vector - time_interval*i/num));
+    marker(i+1)     = index;
 end
 
 
-for j=1:num+1
-    pp   = pQ(II(j),:)';
-    pp2  = pM(II(j),:)';
+for j = 1:num+1
+    
+    % position of quadrotor
+    pp   = pQ(marker(j),:)';
+    
+    % position of load
+    pp2  = pM(marker(j),:)';
 
+    % cable unit vector 
     nn   = (pp - pp2)/norm(pp - pp2);
     
+    % rotation matrix associated to cable
     RR   = [null(nn') nn];
+    
+    % plot quadrotor drawing
     %DISC_plot(pp,RR,1,[0.8 0.8 0.8],0.5,0.1)
+    %plot_vect(pp,pp + RR*[0;0;1]*0.1,[0.8 0.8 0.8])  
     quad_plot(pp,RR,1,[0.5 0.5 0.5],0.1,0.1)
-    Connect(pp',pp2')
-    plot_vect(pp,pp + RR*[0;0;1]*0.1,[0.8 0.8 0.8])     
+    
+    % draw cable between load and quadrotor
+    Connect(pp',pp2')   
 
-    pp2 = pMd(II(j),:)';
+    % load desired position 
+    pp2 = pMd(marker(j),:)';
+    % plot load desired position
     plot3(pp2(1),pp2(2),pp2(3),'o','Color','b')
 
-%     plot3(pMd(II(j),1),pMd(II(j),2),pMd(II(j),3),'o','Color','k')
-    
-% %     e3    = [0;0;1];
-% %     g     = parameters_LoadSuspended.g;
-% %     L     = parameters_LoadSuspended.L;
-% %     n3des = (g*e3 - ades(II(j),:)')/norm(g*e3 - ades(II(j),:)');
-% %     
-% %     quad_plot(pMd(II(j),:)' - n3des*L,RRdes,1,[],0.1,0.1)
-% %     Connect(pMd(II(j),:)' - n3des*L,pMd(II(j),:),[0.5 0.5 0.5])
-% %     
-% %     % quad_plot(pMd(II(j),:)',RRdes,1,[0.5 0.5 0.5],0.1,0.1)
 end
 
 xlabel('x (m)')
@@ -91,10 +309,6 @@ zlabel('z (m)')
 axis tight
 
 view(56,16)
-
-% % Store the frame
-% M_frame(j)=getframe(gcf); % leaving 
-% movie2avi(M_frame,'Trajectories_1.avi');
 
 %%
 close all
--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-<!-- Cheat sheet:-->
-# AuxiliarStuff
+<!-- Cheat sheet -->
+<!-- https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet -->
+# Auxiliar Stuff
 
 1. Open see_in_matlab.m
